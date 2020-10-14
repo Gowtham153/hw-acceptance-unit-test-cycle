5edb15266ce3d290735ab6d51db78e643c734232
EC2 Default User <ec2-user@ip-172-31-44-156.us-east-2.compute.internal>
2020-10-14 07:26:29 +0000

    Completed_part_3

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -2,4 +2,15 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+  
+  def self.similar_find movie_title
+
+    movie_dir = Movie.find_by(title: movie_title).director
+
+    return nil if movie_dir.blank?
+
+    Movie.where(director: movie_dir)
+
+  end
+  
 end
